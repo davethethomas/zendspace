cbe419f17dbf1e7b186a921c9d061d38a64be075
Dave Thomas <davethethomas@gmail.com>
2018-06-23 14:00:09 -0600

    Commit with final route to home

---
+++ module/Album/src/Form/AlbumForm.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidthomas
+ * Date: 6/23/18
+ * Time: 1:31 PM
+ */
+namespace Album\Form;
+
+use Zend\Form\Form;
+
+class AlbumForm extends Form
+{
+    public function __construct($name = null)
+    {
+        // We will ignore the name provided to the constructor
+        parent::__construct('album');
+
+        $this->add([
+            'name' => 'id',
+            'type' => 'hidden',
+        ]);
+        $this->add([
+            'name' => 'title',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Title',
+            ],
+        ]);
+        $this->add([
+            'name' => 'artist',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Artist',
+            ],
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => 'Go',
+                'id'    => 'submitbutton',
+            ],
+        ]);
+    }
+}
